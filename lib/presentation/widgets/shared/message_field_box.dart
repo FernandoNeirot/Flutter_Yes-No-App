81a35326e3424787ac83d7150bd70d2332092974
FeRNaNDo NeiRot <fernando_neirot@itrsa.com.ar>
2024-11-11 20:37:58 -0300

    cierre seccion 5

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,6 +5,41 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField();
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+    final colors = Theme.of(context).colorScheme;
+    final outlineInputBorder = UnderlineInputBorder(
+      borderRadius: BorderRadius.circular(20),
+      borderSide: const BorderSide(color: Colors.transparent),
+    );
+    final inputDecoration = InputDecoration(
+        hintText: 'End your message with a "?"',
+        enabledBorder: outlineInputBorder,
+        focusedBorder: outlineInputBorder,
+        filled: true,
+        suffix: IconButton(
+          icon: const Icon(Icons.send_outlined),
+          onPressed: () {
+            final textValue = textController.value.text;            
+            print("Send message");
+            textController.clear();
+          },
+        ));
+    return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
+        decoration: inputDecoration,
+        onFieldSubmitted: (value) {
+          print("Submit value $value");
+          textController.clear();
+          focusNode.requestFocus();
+        },
+        // onChanged: (value) {
+        //   print("Change value $value");
+        // }
+        );
   }
 }
