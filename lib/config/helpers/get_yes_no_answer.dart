091a60db94b06427ec4082ad571c7b543eb23e2c
FeRNaNDo NeiRot <fernando_neirot@itrsa.com.ar>
2024-11-13 01:07:24 -0300

    add mappers

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -6,6 +6,6 @@ class GetYesNoAnswer {
 
   Future<Message> getAnswer() async{
     final response = await _dio.get('https://yesno.wtf/api');
-    throw UnimplementedError();
+    return Message(text: response.data['answer'], fromWho: FRomWho.her,imageUrl: response.data['image']);
   }
 }
--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,21 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
   final ScrollController scrollController = ScrollController();
-
+  final getYesNoMessage = GetYesNoAnswer();
   List<Message> messageList = [
     Message(text: "Hola", fromWho: FRomWho.me),
     Message(text: "Como estas?", fromWho: FRomWho.me),
   ];
 
   Future<void> sendMessage(String text) async {
-    if(text.isEmpty) return;
+    if (text.isEmpty) return;
     final newMessage = Message(text: text, fromWho: FRomWho.me);
     messageList.add(newMessage);
+    if (text.endsWith("?")) {
+      herRelay();
+    }
     notifyListeners();
     moveScrollToBottom();
   }
+
+  Future<void> herRelay() async {
+    final herMessage = await getYesNoMessage.getAnswer();
+    messageList.add(herMessage);
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
   void moveScrollToBottom() async {
     await Future.delayed(const Duration(milliseconds: 100));
     scrollController.animateTo(
